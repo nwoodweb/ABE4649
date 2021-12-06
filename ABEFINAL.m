88c19aae5f9b493539fad9569c62fead22dc80f4
Nathan Wood <60708568+nwoodweb@users.noreply.github.com>
2021-12-06 12:35:14 -0500

    Update ABEFINAL.m
    updated vector parameters and precalculations

--- ABEFINAL.m
+++ ABEFINAL.m
@@ -4,7 +4,7 @@ clear, close all, format compact, clc
 % The purpose of this project is to model the 3 dimensional "eigenforest"
 % Model created by our group. 
 % The equations detail change in tree population (dx)
-%       change in parasitic beetle population (db) 
+%       change in parasitic beetle population (db, nondim dGamma) 
 %       and change in human lumbar mill policy (dh) 
 %********** PARAMETERS
 %   time: integer (> 0 ) 
@@ -19,6 +19,14 @@ clear, close all, format compact, clc
 %       serves as nondimensional unit alfa
 %   beetleEq: equation 
 %       serves as nondimensional unit B
+%   beetleDamage: float ((0,1])
+%       indicates damage to tree by beetle (1/(beetle*time)) 
+%   P: float ((0,+infty)) 
+%       indicates profitability per tree harvested ($/tree) 
+%   beetleMortality: float ([0,1]) 
+%       indicates natural loss of beetles (1/time) 
+%   tree: ([0,K])
+%       indicates numbers of trees (tree) 
 %********* END PARAMETERS
 
 %SETUP PARAMETERS
@@ -30,46 +38,48 @@ tVector = [1:dt:time]';      % create vector 1-time @ dt increment
 xVector = zeros(size(tVector));
 gammaVector = zeros(size(tVector));
 
-beetleMortality = .2;        % natural beetle death rate 
-rFumigation = .1;            % setup fumigation rate 
-rBeetle = .3;
-rHuman = .12;     
-rTree = .5;
-beetleDamage = .12;
+beetleMortality = .2;       % natural beetle death rate 1/time
+rFumigation = .1;           % fumigation rate 1/time
+rBeetle = .3;               % intrinsic beetle 1/time
+rHuman = .1;               % intrinsic policy 1/ttime
+rTree = .08;                 % intrinisc tree growth 1/time
+beetleDamage = .12;         % beetle damage to tree 1/(beetle * tree)
+tree = 5000;                   % Trees tree
+c = .0024;                     % cost of fumigation $/beetle
+q = 8;                      % num. fumigations dimless
+f = 1;                      %
+P = 4;                      % profitability $/tree
+h = .23;                     % harvest effor 1/$
+K = 3000;                      % tree carry capacity tree
+z = .11;                    % habitability for beetle 1/tree
+H = 12;                     % policy effort $ 
 
-tree = 1;
-c = 24;
-q = 8;
-f = 1;
-P = 4;
-h = 23; 
-K = 5; 
-z = 11;
-H = .12; 
-
+% BEGIN PRECOMPUTATIONS 
 beetleEq = (rFumigation*H)/((z*rBeetle*tree) - (beetleMortality))
+theta = (rHuman*P*h*K)/(rTree)                  %
+phi = (c*q*rHuman*h*f)/(rTree*beetleDamage)     %
+alfa = (beetleEq*beetleDamage)/(rTree)          %
+gamma = (h*H*f)/(rTree)                         %
 
-theta = (rHuman*P*h*K)/(rTree)
-phi = (c*q*rHuman*h*f)/(rTree*beetleDamage)
-alfa = (beetleEq*beetleDamage)/(rTree)
-gamma = (h*H*f)/(rTree)
-
-xVector(1) = 1.2;           % init x condition
-gammaVector(1) = 10.2;
-length(tVector)
+xVector(1) = 900;                               % init x condition
+gammaVector(1) = 55.2;                         % init gamma cond. 
 
 % BEGIN EULER 
-for t = 1:(length(tVector)-1)
-    a = alfa;
-    th = theta;
+for t = 1:(length(tVector)-1)               
+    % for each incremental unit time
+    %    iterate through the provided nondim eqn to populate xVector
+    %    and gammaVector 
+    a = alfa;                               
+    th = theta;         
     p = phi;
     x = xVector(t);
     g = gammaVector(t);
-    xVector(t+1) = xVector(t) + dt*(x*(1-x) - a*x - gamma*x);
-    gammaVector(t+1) = gammaVector(t) + dt*((th*gamma*x) - (p*x));
+    xVector(t+1) = xVector(t) + dt*(x*(1-x) - a*x - gamma*x);      %dTree
+    gammaVector(t+1) = gammaVector(t) + dt*((th*gamma*x) - (p*x)); %dPolicy
 end 
 
-%BEGIN PLOT 
+%PLOTTING SYSTEM 
+
 figure(101)                           %initialize plot
 %ISOCLINE STUFF 
 plot([0 0],[-1 1.2],'b'); hold on
@@ -78,13 +88,17 @@ plot(xx,1-xx,'b');
 % y-isoclines (red)
 plot([-1 1.2],[0 0],'r');
 plot([1 1]*gamma,[-1 1.2],'r');
-%xlim([-0.02 1.1]), ylim([-0.02 1.1])  %establish axis bound        
+%xlim([-0.02 1.1]), ylim([-0.02 1.1])  %establish axis boundaries  
 set(gca,'fontsize',16)
-xlabel('x'), ylabel('y')              %establish axis title
+xlabel('Trees'), ylabel('Policy')              %establish axis title
 grid on
-% Phase portrait avnd endpoints (black)
+
 plot(xVector,gammaVector,'k');        % plot points
+
+% THIS IS THE STARTING PONT 
 plot(xVector(1),gammaVector(1),'ok','markersize',8);
+
+% THIS IS THE END POINT
 plot(xVector(end),gammaVector(end),'pk','markersize',10,'markerfacecolor','k');
 
 %-EOF- 
